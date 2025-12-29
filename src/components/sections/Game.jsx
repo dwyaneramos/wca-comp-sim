fc850875dad5a63245c765e2fdea805e9158c550
riz3ndrr <riz3ndrr@gmail.com>
2025-12-29 15:04:35 +1300

    can toggle visibility of other people's times

--- src/components/sections/Game.jsx
+++ src/components/sections/Game.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 
 
@@ -7,21 +7,30 @@ export const Game = (props) => {
   const [solveNum, setSolveNum] = useState(0)
   const [canViewOtherTimes, setViewOtherTimes] = useState(true)
   const solves = [1,2,3,4,5]
+  const numSolvesInRound = competitors[0].times.length
+  const [ toggleButtonDisabled, setToggleDisability] = useState(false);
+  console.log(competitors)
 
-  for (const c of competitors) {
-    const t = c.times.slice(0, solveNum)
-    console.log(c.name, ": ", Math.min(...t))
-  }
+  useEffect(() => {
+    console.log(solveNum, numSolvesInRound, "AHHH")
+    if (solveNum == numSolvesInRound) {
+      setViewOtherTimes(true);
+      setToggleDisability(true);
+
+    }
+  }, [solveNum])
 
 
-  if (solveNum >= 1 && solveNum <= 4) {
-    competitors.sort(function(c1, c2) {return Math.min(...c2.times.slice(0,solveNum)) - Math.min(...c1.times.slice(0,solveNum))} )
+  if (solveNum >= 1 && solveNum <= numSolvesInRound - 1) {
+    competitors.sort(function(c1, c2) {return Math.min(...c1.times.slice(0,solveNum)) - Math.min(...c2.times.slice(0,solveNum))} )
+  } else if (solveNum == numSolvesInRound) {
+    competitors.sort(function(c1, c2) {return c1.avg - c2.avg})
   }
   return (
     <section className = "flex flex-col pt-20 items-center gap-3  w-screen h-screen bg-white">
       <h1 className="text-3xl">hello</h1> 
       <button type="" onClick={() => setSolveNum(solveNum + 1)} className = "bg-green-500 h-10 p-3 rounded-xl cursor-pointer">gen times</button>
-      <ToggleShowOtherTimes canViewOtherTimes = {canViewOtherTimes} setViewOtherTimes = {setViewOtherTimes}/>
+      <ToggleShowOtherTimes disabled = {toggleButtonDisabled} canViewOtherTimes = {canViewOtherTimes} setViewOtherTimes = {setViewOtherTimes}/>
       <div className="flex flex-col gap-2  overflow-y-scroll">
         <div className = "flex flex-row w-3xl pl-50 py-2 items-center rounded-md border-2 border-gray-200 gap-5">
           {solves.map((s) => {
@@ -46,9 +55,9 @@ export const Game = (props) => {
   )
 }
 
-const ToggleShowOtherTimes = ({canViewOtherTimes, setViewOtherTimes}) =>  {
+const ToggleShowOtherTimes = ({disabled, canViewOtherTimes, setViewOtherTimes}) =>  {
   return (
-    <button type="" className={` ${canViewOtherTimes ? "bg-gray-300 " : "bg-green-300"} relative  w-20 rounded-3xl h-10`} onClick = {() => setViewOtherTimes(!canViewOtherTimes)}>
+    <button type="" disabled = {disabled} className={` ${canViewOtherTimes ? "bg-gray-300 " : "bg-green-300"} relative  w-20 rounded-3xl h-10`} onClick = {() => setViewOtherTimes(!canViewOtherTimes)}>
       <div className = {`${canViewOtherTimes ? "left-1" : "left-[50%]"} transition-all duration-200 absolute rounded-[99px] top-1 w-8 h-8  bg-white`}>
         
       </div>
@@ -84,9 +93,9 @@ const PlayerRow = ({cuber, solveNum, canViewOtherTimes}) => {
       })}
 
 
-      { canViewOtherTimes &&
+      {
         <h1 className = {`${solveNum == cuber.times.length ? "text-black" : "text-gray-500"}`}>
-          {avgToDisplay}
+          {avgToDisplay = canViewOtherTimes ? avgToDisplay : "#####"}
         </h1>
       }
     
