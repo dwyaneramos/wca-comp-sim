cb1c71d0f1c4055a3229f9a4cecb5a0722d59a39
riz3ndrr <riz3ndrr@gmail.com>
2026-01-06 08:57:32 +1300

    adding stats.jsx

---
+++ src/components/sections/Stats.jsx
@@ -0,0 +1,97 @@
+export const Stats = (props) => {
+  const event = props.event
+  const stats = props.stats 
+  const eventStats = stats[event]
+
+  console.log("SDHFNJSDHFJ", eventStats)
+  return (
+    <section className="mt-20 ">
+
+      <h1>Stats Page:</h1>
+      <div className="flex flex-row gap-10">
+        
+        <div className = "flex flex-col">
+          <Top5Section type = {"Averages"} topTimes = {eventStats.bestAvgs}/>
+          <Top5Section type = {"Singles"} topTimes = {eventStats.bestTimes}/>
+        </div>
+        <CompStats eventStats = {eventStats} event = {event}/>
+
+      </div>
+    </section>
+  )
+}
+
+const CompStats = ({eventStats, event}) => {
+  return (
+    <div className = "bg-white h-100 rounded-md p-3 border-2 border-gray-200">
+      <h1 className = "text-lg">Competition Stats for {event}</h1>
+      <h2 className = "text-gray-600">Competitions</h2>
+      <p>Simulated {eventStats.numRoundsDone} rounds</p>
+      
+      <h2 className = "text-gray-600">Total Solves Done</h2>
+      <p>{eventStats.solves.length} solves</p>
+
+      <h2 className = "text-gray-600">Average Placing</h2>
+      <p>{eventStats.avgPlacing.toFixed(2)} out of {eventStats.avgCompetitorsInRound}</p>
+
+
+      <h2 className = "text-gray-600">Podium Count</h2>
+      <div className = "flex flex-row bg-gray-100 gap-5 p-2 items-center justify-center rounded-md">
+        <div className = "flex flex-col items-center ">
+          <span className="text-3xl">🥇</span> 
+          <span className="text-xl">{eventStats.podiumCount[0]}</span>
+        </div>
+
+        <div className = "flex flex-col items-center ">
+          <span className="text-3xl">🥈</span> 
+          <span className="text-xl">{eventStats.podiumCount[1]}</span>
+        </div>
+
+        <div className = "flex flex-col items-center ">
+          <span className = "text-3xl">🥉</span> 
+          <span className = "text-xl">{eventStats.podiumCount[2]}</span>
+        </div>
+        
+      </div>
+
+
+    </div>
+  )
+}
+
+const Graph = () => {
+  return (
+    <div className="bg-red-200 w-200 h-200">
+      aj
+    </div>
+  )
+}
+
+
+const Top5Section = ({type, topTimes}) => {
+  const rankColors = {0 : "bg-[#ebcc34]",
+                      1 : "bg-[#9e9e9e]",
+                      2 : "bg-[#f2a750]",
+                      3 : "bg-[#d1d1d1]",
+                      4 : "bg-[#d1d1d1]"}
+
+
+  return (
+    <div className = "w-2xs flex pt-3 flex-col items-center h-90 bg-white rounded-md border-2 border-gray-200 mb-10">
+      <h1 className = "text-xl mb-3">Top 5 {type}:</h1>
+      
+      <div className = "flex flex-col gap-2">
+        
+        {topTimes.map((time, idx) => {
+          return (
+            <div key={idx} className={`flex place-content-between text-md rounded-xl  w-50 p-2 ${rankColors[idx]}`}>
+              <span className = {`${rankColors[idx]} rounded-[100px] mr-2`}>{idx + 1}</span> 
+              <span>{time.toFixed(2)}</span>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+
+}
