e17eedd1b23e067a13d92d808adf4a0b0afdac53
riz3ndrr <riz3ndrr@gmail.com>
2026-01-29 10:16:54 +1300

    Revert "new way of url"
    This reverts commit a7f2b4e1d1312384daad6a1e7a0c4f8a95c771cb.

--- src/components/services/searchCubers.js
+++ src/components/services/searchCubers.js
@@ -1,9 +1,8 @@
 import {isCuberInList} from '../services/competitors.js'
 
 export const searchCubers = async (searchInput, competitors) => {
-console.log("BASE_URL:", import.meta.env.BASE_URL)  // <-- add this line here
   let results = [] 
-  const dataURL = new URL('cubers.json', import.meta.env.BASE_URL)
+  const dataURL = `${import.meta.env.BASE_URL}cubers.json`
   const data = await fetch(dataURL)
   const dataJSON = await data.json()
 
