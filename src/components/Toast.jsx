4d34564a08bc25c59b1859ad3d6803bf1a347816
dra131 <dra131@uclive.ac.nz>
2026-04-01 13:06:06 +1300

    feat: toast animation when exiting

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react"
+
 const typeConfigLookup = {
   "error": { bgColor: "bg-red-500", textColor: "text-white", header: "Uh Oh!" },
 }
@@ -5,13 +7,41 @@ const typeConfigLookup = {
 
 
 export const Toast = ({ text, type, setShowToast }) => {
+  const [timeElapsed, setTimeElapsed] = useState(0)
+  const [isExiting, setIsExiting] = useState(false)
+  const delayBeforeExit = 500
+
+  useEffect(() => {
+
+    const interval = setInterval(() => {
+      setTimeElapsed(prev => prev + 1)
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [])
+
+  useEffect(() => {
+
+
+    if (timeElapsed > 2) {
+      setIsExiting(true)
+      console.log(timeElapsed, isExiting)
+      setTimeout(() => setShowToast(null), delayBeforeExit)
+    }
+  }, [timeElapsed, isExiting])
+
+
   const typeConfig = typeConfigLookup[type]
-  console.log(typeConfig)
+  const animation = isExiting ? "animate-toast-exit" : "animate-toast-enter"
   return (
-    <div className={`fixed transition-all left-1/2 -translate-x-1/2 animate-toast flex flex-col bottom-5 z-100 w-xs ${typeConfig.bgColor} ${typeConfig.textColor} p-3 rounded-md shadow-lg`} >
+    <div className={`fixed transition-all left-1/2 -translate-x-1/2 ${animation} flex flex-col bottom-5 z-100 w-xs ${typeConfig.bgColor} ${typeConfig.textColor} p-3 rounded-md shadow-lg`}
+      onClick={() => {
+        setIsExiting(true)
+        setTimeout(() => setShowToast(null), delayBeforeExit)
+      }}>
       <span className="text-lg">{typeConfig.header}</span>
       <span className="text-md">{text}</span>
-      <span onClick={() => setShowToast(null)} className="transition-all pr-1 absolute top-2 right-2 text-white text-lg cursor-pointer hover:-translate-y-1">x</span>
+      <span className="transition-all pr-1 absolute top-2 right-2 text-white text-lg cursor-pointer hover:-translate-y-1">x</span>
     </div >
   )
 }
